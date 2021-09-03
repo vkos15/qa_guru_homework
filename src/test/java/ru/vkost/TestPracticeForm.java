4a10434f73ba9883bc0138ba3c9387513afa4b84
Valentina <15valentinka@gmail.com>
2021-09-03 07:45:08 +0500

    +test

--- src/test/java/ru/vkost/TestPracticeForm.java
+++ src/test/java/ru/vkost/TestPracticeForm.java
@@ -3,11 +3,11 @@ package ru.vkost;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 
-import static com.codeborne.selenide.Selectors.byName;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import java.io.File;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
 
 public class TestPracticeForm {
 
@@ -18,12 +18,37 @@ public class TestPracticeForm {
     @Test
     void fillFormTest(){
 
-      //  open("https://demoqa.com/automation-practice-form");
-        open("https://demoqa.com/text-box");
+        open("https://demoqa.com/automation-practice-form");
+       // open("https://demoqa.com/text-box");
         $("#firstName").setValue("Valentina");
         $("#lastName").setValue("KUlikova");
-        $("userEmail").setValue("jrhdgdfg@mail.ru");
-        $(byName("gender")).click();
+        $("#userEmail").setValue("jrhdgdfg@mail.ru");
+       // $(byName("gender")).parent().click();
+       // $("input[name='gender'] label").click();
+        $("label[ for='gender-radio-1']").click();
+        $("#userNumber").setValue("89823455666");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOptionContainingText("June");
+        $(".react-datepicker__year-select").selectOptionContainingText("1992");
+        $(".react-datepicker__month-container").$(byText("28")).click();
+
+        //$(".subjects-auto-complete__input").click();
+        $("#subjectsInput").sendKeys("m");
+        //.sendKeys("m");
+       // $(".subjects-auto-complete__menu-list").$(byText("Maths")).click();
+        $(".subjects-auto-complete__menu").$(byText("Maths")).click();
+        $("label[for='hobbies-checkbox-1']").click();
+        $("#uploadPicture").uploadFile(new File("src/test/resources/photo_test.jpg"));
+        $("#currentAddress").setValue("Perm");
+   //     $("#submit").click();
+//$x("//div[text()='Select State']").click();
+        $(byText("Select State")).click();
+        $("#state").click();
+    //$(byText("NCR")).click();
+        $("#react-select-3-option-0").click();
+
+        sleep(10000);
+
 
 
     }
