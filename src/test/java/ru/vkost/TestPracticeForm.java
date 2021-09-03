d79333ebcab89b78d90ea1150a34040161b9bee3
Валентина <15valentinka@gmail.com>
2021-09-03 15:18:25 +0500

    gitignore

--- src/test/java/ru/vkost/TestPracticeForm.java
+++ src/test/java/ru/vkost/TestPracticeForm.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import java.io.File;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -15,18 +16,23 @@ public class TestPracticeForm {
     static void beforeAll() {
         Configuration.startMaximized = true;
     }
+
     @Test
-    void fillFormTest(){
+    void practiceFormTest() {
 
         open("https://demoqa.com/automation-practice-form");
-       // open("https://demoqa.com/text-box");
-        $("#firstName").setValue("Valentina");
-        $("#lastName").setValue("KUlikova");
-        $("#userEmail").setValue("jrhdgdfg@mail.ru");
-       // $(byName("gender")).parent().click();
-       // $("input[name='gender'] label").click();
+        String firstName = "Valentina";
+        String address = "Perm";
+        String lastName = "Kulikova";
+        String email = "jrhdgdfg@mail.ru";
+        String phoneNumber = "89823455666";
+
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(email);
+        // $(byName("gender")).parent().click();
         $("label[ for='gender-radio-1']").click();
-        $("#userNumber").setValue("89823455666");
+        $("#userNumber").setValue(phoneNumber);
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOptionContainingText("June");
         $(".react-datepicker__year-select").selectOptionContainingText("1992");
@@ -34,25 +40,22 @@ public class TestPracticeForm {
 
         //$(".subjects-auto-complete__input").click();
         $("#subjectsInput").sendKeys("m");
-        //.sendKeys("m");
-       // $(".subjects-auto-complete__menu-list").$(byText("Maths")).click();
+        // $(".subjects-auto-complete__menu-list").$(byText("Maths")).click();
         $(".subjects-auto-complete__menu").$(byText("Maths")).click();
         $("label[for='hobbies-checkbox-1']").click();
         $("#uploadPicture").uploadFile(new File("src/test/resources/photo_test.jpg"));
-        $("#currentAddress").setValue("Perm");
-   //     $("#submit").click();
-//$x("//div[text()='Select State']").click();
-        $(byText("Select State")).click();
+        $("#currentAddress").setValue(address);
         $("#state").click();
-    //$(byText("NCR")).click();
-        $("#react-select-3-option-0").click();
-
-        sleep(10000);
-
+        $(byText("NCR")).click();
+        $("#react-select-4-input").setValue("Moscow");
+        $("#submit").click();
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        //$("tr").$(byText("Label")).click();
+        $(".table-responsive tbody tr td:nth-child(1)").shouldHave(text("Student Name"));
+        $(".table-responsive tbody tr td:nth-child(2)").shouldHave(text("Valentina KUlikova"));
 
 
     }
-
 
 
 }
