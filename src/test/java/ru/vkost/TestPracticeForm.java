5d94f2a30924fb0aa88d5460363e299ae49f869e
Valentina <15valentinka@gmail.com>
2021-09-03 23:38:18 +0500

    +check table

--- src/test/java/ru/vkost/TestPracticeForm.java
+++ src/test/java/ru/vkost/TestPracticeForm.java
@@ -25,7 +25,7 @@ public class TestPracticeForm {
         String address = "Perm";
         String lastName = "Kulikova";
         String email = "jrhdgdfg@mail.ru";
-        String phoneNumber = "89823455666";
+        String phoneNumber = "9823455660";
 
         $("#firstName").setValue(firstName);
         $("#lastName").setValue(lastName);
@@ -47,12 +47,27 @@ public class TestPracticeForm {
         $("#currentAddress").setValue(address);
         $("#state").click();
         $(byText("NCR")).click();
-        $("#react-select-4-input").setValue("Moscow");
+        $("#react-select-4-input").setValue("Delhi").pressEnter();
+
         $("#submit").click();
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
-        //$("tr").$(byText("Label")).click();
         $(".table-responsive tbody tr td:nth-child(1)").shouldHave(text("Student Name"));
         $(".table-responsive tbody tr td:nth-child(2)").shouldHave(text("Valentina KUlikova"));
+        $x("//td[text()='Student Email']/following-sibling::td").shouldHave(text(email));
+        $x("//td[text()='Gender']/following-sibling::td").shouldHave(text("Male"));
+        $x("//td[text()='Mobile']/following-sibling::td").shouldHave(text(phoneNumber));
+        $x("//td[text()='Date of Birth']/following-sibling::td").shouldHave(text("28 June,1992"));
+        $x("//td[text()='Subjects']/following-sibling::td").shouldHave(text("Maths"));
+        $x("//td[text()='Hobbies']/following-sibling::td").shouldHave(text("Sports"));
+        $x("//td[text()='Picture']/following-sibling::td").shouldHave(text("photo_test.jpg"));
+        $x("//td[text()='Address']/following-sibling::td").shouldHave(text(address));
+        $x("//td[text()='State and City']/following-sibling::td").shouldHave(text("Delhi"));
+
+
+
+
+
+
 
 
     }
