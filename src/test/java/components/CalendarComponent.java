f68a097ee278b45c1a73e85065b75eae0261aac7
Valentina <15valentinka@gmail.com>
2021-09-12 22:39:20 +0500

    remove extra spaces

--- src/test/java/components/CalendarComponent.java
+++ src/test/java/components/CalendarComponent.java
@@ -1,11 +1,10 @@
 package components;
 
-
 import static com.codeborne.selenide.Selenide.$;
+
 public class CalendarComponent {
 
     public void setDate(String day, String month, String year){
-
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOptionContainingText(month);
         $(".react-datepicker__year-select").selectOptionContainingText(year);
